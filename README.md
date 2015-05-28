4c74e753bc1d060cc7afeea7b7f5204a6cef816c
Glen Sawyer <github@snelg.com>
2015-05-28 13:47:23 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cors plugin for CakePHP
+
+A simple plugin to add CORS headers to specified requests.
+
+In bootstrap.php, add something like
+
+DispatcherFactory::add('Cors.Cors', ['routes' => [
+    'ControllerClassName' => 'some_action',
+    'OtherControllerClassName' => [
+        'action_one' => ['origin' => 'somesite.com'],
+        'action_two'],
+    'ThirdController]]);
+
+
+## Installation
+
+You can install this plugin into your CakePHP application using [composer](http://getcomposer.org).
+
+The recommended way to install composer packages is:
+
+```
+composer require your-name-here/Cors
+```
---
+++ src/Routing/Filter/CorsFilter.php
@@ -0,0 +1,54 @@
+<?php
+namespace Cors\Routing\Filter;
+
+use Cake\Event\Event;
+use Cake\Routing\DispatcherFilter;
+
+class CorsFilter extends DispatcherFilter {
+    public function beforeDispatch(Event $e) {
+        $request = $e->data['request'];
+        $controller = $request->param('controller');
+        $action = $request->param('action');
+        $handledRoutes = $this->config('routes');
+
+        if (empty($handledRoutes[$controller])) {
+            //Might be numeric-keyed single entry
+            if (in_array($controller, $handledRoutes)) {
+                $handledRoutes[$controller] = ['*'];
+            } else {
+                return;
+            }
+        }
+        
+        if (!is_array($handledRoutes[$controller])) {
+            //Single actions can be a string
+            $handledRoutes[$controller] = [$handledRoutes[$controller]];
+        }
+        
+        if (empty($handledRoutes[$controller][$action]) &&
+                !in_array($action, $handledRoutes[$controller]) && 
+                !in_array('*', $handledRoutes[$controller])) {
+            //Not this action
+            return;
+        }
+        
+        $origin = '*';
+        $methods = [];
+        $headers = [];
+
+        if (!empty($handledRoutes[$controller][$action])) {
+            $params = $handledRoutes[$controller][$action];
+            if (!empty($params['origin'])) {
+                $origin = $params['origin'];
+            }
+            if (!empty($params['methods'])) {
+                $methods = $params['methods'];
+            }
+            if (!empty($params['headers'])) {
+                $headers = $params['headers'];
+            }
+        }
+        
+        $e->data['response']->cors($request, $origin, $methods, $headers);
+    }
+}
